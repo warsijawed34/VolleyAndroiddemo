0f49a1193e3c1096ba390440b85dafad9fcb0521
warsijawed34 <warsijawed34@gmail.com>
2017-08-05 17:55:09 +0530

    second commit

--- app/src/main/java/volleyandroiddemo/com/volleyandroiddemo/SimpleRequest.java
+++ app/src/main/java/volleyandroiddemo/com/volleyandroiddemo/SimpleRequest.java
@@ -1,8 +1,11 @@
 package volleyandroiddemo.com.volleyandroiddemo;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +18,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
@@ -26,9 +30,11 @@ public class SimpleRequest extends AppCompatActivity implements View.OnClickList
     private Button btnfetch, btnRQueue;
     private TextView tvDisplay;
     private ImageView ivDisplay;
-    private String url_contact = "http://www.google.com";
+    private String url_simple = "http://www.google.com";
+    private String url_simple1 = "https://api.androidhive.info/volley/string_response.html";
     private String url_image = "https://s-media-cache-ak0.pinimg.com/736x/35/04/c6/3504c6e12e58d5571eb88830f7bc4f7c--deepika-padukone-hair-dipika-padukone.jpg";
     private RequestQueue requestQueue;
+    private String TAG = SimpleRequest.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,21 +55,21 @@ public class SimpleRequest extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnFetch:
-                // simpleRequest();
-                // singletonClassMethod();
                 requestImages();
                 break;
             case R.id.btnRqueue:
-                setRequestQueue();
+                Intent intent = new Intent(SimpleRequest.this, ImageRequestActivity.class);
+                startActivity(intent);
                 break;
             default:
                 break;
         }
     }
 
+    // simple request from google.com
     private void simpleRequest() {
         requestQueue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url_contact, new Response.Listener<String>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url_simple, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 tvDisplay.setText("Response is: " + response.substring(0, 500));
@@ -81,12 +87,13 @@ public class SimpleRequest extends AppCompatActivity implements View.OnClickList
 
     }
 
+    //using RequestQueue, network, and cache
     private void setRequestQueue() {
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
         Network network = new BasicNetwork(new HurlStack());
         requestQueue = new RequestQueue(cache, network);
         requestQueue.start();
-        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url_contact, new Response.Listener<String>() {
+        StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url_simple, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 tvDisplay.setText("Response is: " + response.substring(0, 500));
@@ -103,9 +110,10 @@ public class SimpleRequest extends AppCompatActivity implements View.OnClickList
 
     }
 
+    //using singleton class here..
     private void singletonClassMethod() {
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url_contact, new Response.Listener<String>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url_simple, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 tvDisplay.setText("Response is: " + response.substring(0, 500));
@@ -119,6 +127,7 @@ public class SimpleRequest extends AppCompatActivity implements View.OnClickList
         MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 
+    //fetching image_url here...
     private void requestImages() {
 
         ImageRequest imageRequest = new ImageRequest(url_image, new Response.Listener<Bitmap>() {
